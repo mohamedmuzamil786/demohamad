8830abfa38c8fe14100c197316e9354af6667246
mohamedmuzamil786 <moahmedmuzamil03@gmail.com>
2021-01-20 20:12:56 +0530

    done

---
+++ src/org/hamad/ClassA.java
@@ -0,0 +1,18 @@
+package org.hamad;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=30;
+		int d=40;
+		System.out.println("Sffffffffffffff");
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println("sf");
+		System.out.println("code");
+	}
+}
